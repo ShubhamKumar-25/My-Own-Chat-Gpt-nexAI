8a2dbde4239cb409133f863a4cbbd355c56ab625
Shubham <sk5989229@gmail.com>
2026-07-02 01:26:39 +0530

    API URL changed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
-
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api/chat' });
+const API = axios.create({ baseURL: 'https://my-own-chat-gpt-nexai.onrender.com/api/chat' });
 
-// Pehle wale functions ko update karein
 export const sendMessage = (messageData) => API.post('/send', messageData);
 export const fetchHistory = (userId) => API.get(`/history?userId=${userId}`);
 export const deleteChat = (userId) => API.delete(`/delete?userId=${userId}`);
