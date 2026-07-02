fd7d9690d180e48fef2566970d30950357fafbdb
Shubham <sk5989229@gmail.com>
2026-07-02 14:36:39 +0530

    Both server are changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,44 @@
+// import axios from 'axios';
+
+// const api = axios.create({ 
+    
+//   baseURL: 'https://my-own-chat-gpt-nexai.onrender.com/api' 
+// });
+
+// export default api;
+
+
+
+
+
+
+
+
+
+
+
 import axios from 'axios';
 
-const api = axios.create({ 
-    
-  baseURL: 'https://my-own-chat-gpt-nexai.onrender.com/api' 
+const API_URL = 'https://my-own-chat-gpt-nexai.onrender.com/api'; 
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
