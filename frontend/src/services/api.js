84b3ab40ac04ce90a30b0b37ece7904ec39cab9b
Shubham <sk5989229@gmail.com>
2026-07-02 12:53:18 +0530

    Auth correction

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({ 
-  // Base URL mein hi humne Render ka sahi live domain aur '/api' jod diya hai
+    
   baseURL: 'https://my-own-chat-gpt-nexai.onrender.com/api' 
 });
 
